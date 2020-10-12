eddf980667e7af6cdecaeff89c56adfc88c48865
Kerry Cheng <kecheng@google.com>
2020-10-12 19:03:30 -0400

    Add react deps Add an example query to about page This page hits the local GraphQL endpoint from the federation demo

--- containers/kerry-react/src/About.js
+++ containers/kerry-react/src/About.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {useQuery} from '@apollo/react-hooks';
+import gql from "graphql-tag";
+
+const GET_ADA_USER = gql`
+{
+  user(username: "@ada") {
+    name
+  }
+}`
 
 export function About() {
+  const {data, loading, error} = useQuery(GET_ADA_USER);
+
   return (
     <div className="About">
       MOOOOOOOSE
+      {loading ? <p>Loading...</p> : data && data.user && data.user.name}
     </div>
   );
 }
