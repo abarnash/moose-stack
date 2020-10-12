f34216eb6f2f6f474d30c7e1adc9552a67a590de
Kerry Cheng <kecheng@google.com>
2020-10-12 17:34:25 -0400

    Replace shitty navbar with bootstrap one Add about page

--- containers/kerry-react/src/App.js
+++ containers/kerry-react/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Link, Route, BrowserRouter, Switch} from 'react-router-dom';
+import Navbar from 'react-bootstrap/navbar';
+import Nav from 'react-bootstrap/nav';
+import {Route, BrowserRouter, Switch} from 'react-router-dom';
 import './App.css';
 import {Home} from './Home';
 import {About} from './About';
@@ -7,32 +9,24 @@ import {About} from './About';
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">Online Moose</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
