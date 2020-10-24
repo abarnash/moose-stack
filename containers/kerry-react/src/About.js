e57f142ad9ad72817f827e4d719e66aa02b74b54
Kerry Cheng <kecheng@google.com>
2020-10-24 15:20:22 -0400

    Update the title and favicon to moose stuff Add info to the about page

--- containers/kerry-react/src/About.js
+++ containers/kerry-react/src/About.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 export function About() {
   return (
-    <div className="About">
-      MOOOOOOOSE
+    <div class="container-fluid">
+      <h3>
+        Rules
+      </h3>
+      <p>
+        Bounce a quarter off the table! If it lands in the right side of
+        the tray, take that many sips. If it lands in the left side, give
+        that many sips to someone else. If it lands in the pot, the last
+        person to moose drinks the whole pot. If anyone false mooses, they
+        have to drink the pot.
+      </p>
+      <h3>
+        Credits
+      </h3>
+      <p>
+        Kerry "The Moose Czar" Cheng
+      </p>
     </div>
   );
 }
