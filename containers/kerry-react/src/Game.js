5ac9fa5b31a3ec281c3e19a3cc30c59ece264dcc
Kerry Cheng <kecheng@google.com>
2020-10-24 14:07:58 -0400

    Fetch game from URL for game page Remove example query stuff from about page

--- containers/kerry-react/src/Game.js
+++ containers/kerry-react/src/Game.js
@@ -1,11 +1,33 @@
+import {useQuery} from '@apollo/react-hooks';
+import gql from "graphql-tag";
 import React, {useState} from 'react';
+import {useParams} from 'react-router-dom';
+
+const GET_GAME = gql`
+  query Game($url: String!) {
+    game(url: $url) {
+      id
+    }
+  }
+`;
 
 export function Game() {
   const [state, setState] = useState({foo: 'bar'});
   //setState({foo: 'baz'});
+  const {gameName} = useParams();
+  const {data, loading, error} = useQuery(GET_GAME, {
+    variables: {url: gameName},
+  });
+
+  if (loading) {
+    return (<p>Loading...</p>);
+  }
+
   return (
     <div>
-      GAME DATA??
+      <div>
+        Game ID: {data.game.id}
+      </div>
     </div>
   );
 }
